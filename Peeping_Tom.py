6eb21a6629260a7098902b1b24da102b60a04268
SeedyFella <65099255+SeedyFella@users.noreply.github.com>
2020-05-11 21:47:53 +1000

    Update Peeping_Tom.py

--- Peeping_Tom.py
+++ Peeping_Tom.py
@@ -4,11 +4,21 @@ import string
 from tkinter import *
 import tkinter as tk
 
-def peep():
-    # size of string
+
+def peep_old():
+    # older sreenshots
     n = 6
     res = ''.join(random.choices(string.ascii_lowercase + string.digits, k=n))
-    webbrowser.open("http://prnt.sc/{}".format(res), new=2)
+    web = webbrowser
+    web.open("https://prnt.sc/{}".format(res), new=2)
+
+
+def peep_new():
+    # newer screenshots
+    n = 3
+    res = ''.join(random.choices(string.ascii_lowercase + string.digits, k=n))
+    web = webbrowser
+    web.open("https://prnt.sc/sem{}".format(res), new=2)
 
 
 # create instance
@@ -17,12 +27,21 @@ win = tk.Tk()
 # title of window
 win.title("Peeping Tom")
 # size of window
-win.geometry("270x200")
+win.geometry("300x200")
 win.resizable(width=False, height=False)
-# label on window
-label1_1 = tk.Button(win, text="Click to peep", bd=1
-                     , relief="solid", bg='#34d5eb', font="Times 32", width=15, height=4, command=peep)
-label1_1.pack()
+win.configure(bg='#03a5fc')
+
+# ALL SCREENSHOTS
+button1_1 = tk.Button(win, text="All Screenshots", bd=1
+                      , relief="solid", font="Times 21", width=15, height=1, command=peep_old)
+button1_1.pack()
+button1_1.place(bordermode=OUTSIDE, height=75, width=250, x=25, y=100)
+
+# RECENT SCREENSHOTS
+button1_2 = tk.Button(win, text="Recent Screenshots", bd=1
+                      , relief="solid", font="Times 21", width=15, height=1, command=peep_new)
+button1_2.pack()
+button1_2.place(bordermode=OUTSIDE, height=75, width=250, x=25, y=15)
 
 # forever loops GUI
 win.mainloop()
